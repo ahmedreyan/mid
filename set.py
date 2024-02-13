cb120c3e47248ec4c746891296e4082657e776e1
Code_Maestro33 <144579360+CodeMaestro33@users.noreply.github.com>
2024-02-13 10:28:39 +0530

    set-mid

---
+++ set.py
@@ -0,0 +1,19 @@
+#Set is Created cruly Brases
+fruits_set={'Apple','orange','kiwi'}
+#Get value
+print(fruits_set)
+#Check if in set 
+print('Apple' in fruits_set)#it tells wheather Apple is there or not it gives value true if it present if it doesn't false
+#Add Set
+fruits_set.add('Grape')
+print(fruits_set)
+#Remove from set
+fruits_set.remove('kiwi')
+print(fruits_set)
+#Delete set
+del fruits_set #not exits Not Defined
+#Add duplicate
+fruits_set.add("Apple")
+print(fruits_set)
+#Clear set
+fruits_set.clear()
