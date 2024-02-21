1e5839cafe94e0047449b59beb92fbfb762bad7f
CodeMaestro33 <144579360+CodeMaestro33@users.noreply.github.com>
2024-02-21 07:17:16 +0000

    py_proj2

---
+++ py_proj02.py
@@ -0,0 +1,12 @@
+#PROGRAM TO CREATERA A DICTIONARY 
+dict={}
+print("Empty Dictionary")
+print(dict)
+#CREATING A DICTIONARY WITH DICT() METHOD
+dict=({1:"Mon",2:"Tue",3:"Wed"})
+print("Dictionary with the use of dict()")
+print(dict)
+#CREATING A DICTIONARY WITH EACH ITEMS AS A PAIR
+dict([(1, "January"),(2, "February ")])
+print("Dictionary with each items as a pair: ")
+print(dict)
