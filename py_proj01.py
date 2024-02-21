bc21d1c6a1c4c28e4b57e76d703242b1059e81b1
CodeMaestro33 <144579360+CodeMaestro33@users.noreply.github.com>
2024-02-21 07:01:30 +0000

    proj_1

---
+++ py_proj01.py
@@ -0,0 +1,16 @@
+#PROGRAM TO CREATE A LIST
+list=[]
+print("Blank list: ")
+#CREATING A LIST OF NUMBER
+list=[10,20,30,40,50,60]
+print("\n list numbers: ")
+print(list)
+#CREATE A LIST OF STRINGS
+list=["Mon","Tue","Wed"]
+print("/nList items: ")
+print(list[0])
+print(list[1])
+#CREATING MULTI DIMENSIONAL LIST
+list=[["Jan","Feb"],["Mar"]]
+print("\nMulti dimensional list ")
+print(list)
