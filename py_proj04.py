ebfcd69d7c3492a1a9fb48c0685b06e562a11a80
CodeMaestro33 <144579360+CodeMaestro33@users.noreply.github.com>
2024-02-22 07:23:04 +0000

    py_set

---
+++ py_proj04.py
@@ -0,0 +1,10 @@
+set1=set()
+set2=set()
+#ADDING ELEMENT TO SET1
+for i in range(1,6):
+    set1.add(i)
+#ADDING ELEMENT TO SET2
+for i in range(3,8):
+    set2.add(i)
+print("Set1= ",set1)
+print("Set2= ",set2)
